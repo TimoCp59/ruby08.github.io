1ba8fc194b79bfc62a271a7b464fb5a665937acd
timocp59 <timothee.capon@gmail.com>
2020-04-08 15:37:02 +0200

    Premier exo

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -1,1 +1,32 @@
+def say_hello
+	puts "Tu n'as pas dit bonjour ! Du coup ... Dis bonjour" 
+end
 
+def perform
+	puts say_hello
+end
+
+perform
+
+def say_hello(first_name)
+	puts "Tu n'as pas dit bonjour ! Du coup ... Dis bonjour #{first_name}" 
+end
+
+def perform
+	puts say_hello("Tim")
+end
+
+perform
+
+def say_hello
+	puts "Quel est ton nom mon petit ?"
+	print ">"
+	first_name = gets.chomp
+	puts "Tu n'as pas dit bonjour ! Du coup ... Dis bonjour #{first_name}" 
+end
+
+def perform
+	puts say_hello
+end
+
+perform
